43cdb2a51a0b8bb0a5def6f36b3472b155117dc1
shazzadhsn <shazzadmail1@gmail.com>
2026-01-06 21:07:03 +0600

    Spiral-Matrix-Problem

---
+++ 2D-Arrays/Spiral_Matrix.java
@@ -0,0 +1,50 @@
+public class Spiral_Matrix {
+    public static void spiralMatrix(int arr[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = arr.length-1;
+        int endCol = arr[0].length-1;
+
+        while(startRow <= endRow && startCol <= endCol) {
+            // top
+            for(int j=startCol; j<=endCol; j++) {
+                System.out.print(arr[startRow][j] + " ");
+            }
+
+            // right
+            for(int i=startRow+1; i<=endRow; i++) {
+                System.out.print(arr[i][endCol] + " ");
+            }
+
+            // bottom
+            for(int j=endCol-1; j>= startCol; j--) {
+                if(startRow == endRow) {
+                    break;
+                }
+                System.out.print(arr[endRow][j] + " ");
+            }
+
+            // left
+            for(int i=endRow-1; i>startRow; i--) {
+                if(startCol == endCol) {
+                    break;
+                }
+                System.out.print(arr[i][startCol] + " ");
+            }
+
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+
+        }
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        spiralMatrix(matrix);
+    }
+}
